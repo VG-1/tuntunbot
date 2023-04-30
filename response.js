8c8241211089f0723c6479f2641668e432c8a30c
Vinayak_Gavariya <vinayakgavariya123@gmail.com>
2023-04-30 19:11:14 +0530

    first commit

---
+++ response.js
@@ -0,0 +1,6 @@
+const responseObj = {
+  hello: "Hey Nimesh ! How are you doing ?",
+  hey: "Hey Nimesh! What's Up",
+  today: new Date().toDateString(),
+  time: new Date().toLocaleTimeString(),
+};

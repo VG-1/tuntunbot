9a1736896317ebac1ab3aa6c488d74bdde2bd77a
Vinayak_Gavariya <vinayakgavariya123@gmail.com>
2023-04-30 19:54:44 +0530

    updated response.js

--- response.js
+++ response.js
@@ -1,6 +1,7 @@
 const responseObj = {
-  hello: "Hey Nimesh ! How are you doing ?",
-  hey: "Hey Nimesh! What's Up",
+  hello: "Hey! How are you doing ?",
+  hey: "Hey! What's Up",
   today: new Date().toDateString(),
   time: new Date().toLocaleTimeString(),
+  bye: "Have a great day, a head :)"
 };
